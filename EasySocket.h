f3c069735c71d80e42f8a44c138b689c35108327
yippiez <erencemayez@gmail.com>
2020-10-14 01:40:31 +0300

    organized header

--- EasySocket.h
+++ EasySocket.h
@@ -8,9 +8,6 @@
 
 // INADDR_ANY is used for any ip addres 0.0.0.0
 
-#define STRLEN(x) (sizeof(x)-1)
-
-
 extern "C"{
 #include "lwip/tcp.h"
 #include "lwip/pbuf.h"
@@ -22,17 +19,24 @@ extern "C"{
 class TCPSocket{
     public:
         TCPSocket();
+
+        // Basic api
         err_t connect(IPAddress ip, uint16_t port);
+        err_t bind(IPAddress ip, uint16_t port);
         err_t send(const void* data, uint16_t len);
         err_t send(const void* data);
-        err_t _recv(tcp_pcb* pcb, pbuf* p, err_t err);
-        static err_t recv_callback(void* arg, tcp_pcb* pcb, pbuf* p, err_t err);
         char read();
         err_t read(char* dst);
+        
+        // Callbacks
+        static err_t recv_callback(void* arg, tcp_pcb* pcb, pbuf* p, err_t err);
+        
+        // other
         err_t data_available();
     
     private:
         void _update_buffer(size_t size);
+        err_t _recv(tcp_pcb* pcb, pbuf* p, err_t err);
         tcp_pcb *tcp_socket;
         uint16 _rx_pbuf_offset;
         pbuf *_rx_pbuf;
